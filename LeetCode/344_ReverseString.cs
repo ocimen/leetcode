0c4f4d75fe907812fa11cdeb7fec6c69ef23adab
Ozge Cimendere <uyelik@ocimen.com>
2020-12-21 22:04:20 +0100

    Add 344 Reverse String

---
+++ LeetCode/344_ReverseString.cs
@@ -0,0 +1,13 @@
+namespace LeetCode {
+    public class Solution {
+        public void ReverseString(char[] s) {
+            int n = s.Length;
+            char temp;
+            for(int i=0; i < s.Length / 2; i++, n--) {
+                temp = s[i];
+                s[i] = s[n-1];
+                s[n-1] = temp;
+            }       
+        }
+    }
+}
